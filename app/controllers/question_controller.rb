62b2157594f963bd6eadd8127bae32f3eb5c243e
github username <github email address>
2021-03-23 09:16:31 -0700

    changes duration of video

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -12,7 +12,7 @@ class QuestionController < ApplicationController
         @question = Question.video_array   
         
         processed_link = @question.gsub('watch?v=', 'embed/')
-        @vid = processed_link+"?autoplay=1&start=#{rand(30..35)}&end=#{rand(45..55)}&cc_load_policy=1"
+        @vid = processed_link+"?autoplay=1&start=#{rand(25..35)}&end=#{rand(45..55)}&cc_load_policy=1"
         # binding.pry
         erb :show
 
